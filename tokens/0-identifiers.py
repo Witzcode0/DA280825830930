c7a96e6e1fa9c5da75233a75b82138fe2eb0e13c
Witzcode0 <brijesh.gondaliya07@gmail.com>
2025-08-28 21:31:26 +0530

    demo lecture

---
+++ tokens/0-identifiers.py
@@ -0,0 +1,40 @@
+# An identifier is a name given to entities like variables, functions, classes, modules, and other objects in Python. It is used to uniquely identify them.
+
+# Rules of identifiers:
+# 1] [a-z, A-Z, 0-9, _]
+# 2] you can not start with the digits
+# Example:
+# 1_level, level1
+# 3] special symbols are not allowed [!,@, #, $, %, ^, ,./|+-]
+# 4] white space are not allowed 
+# Example:
+# first name, firstname, Firstname, FIRSTNAME, FirstName, first_name
+# 5] It must be unique
+# firstname, Firstname, FIRSTNAME, FirstName, first_name
+# 6] keywords are not allowed
+
+
+# Case:
+
+# snack_case:
+# area_of_circle, first_name
+
+# camleCase
+# areaOfCircle, firstName
+
+# PascalCase
+# AreaOfCircle, FirstName
+
+
+# Valid Identifiers
+
+# first_name
+# level_1
+# _level_1
+
+
+# Invalid Identifiers
+# first name
+# for 
+# 1_levle
+# @BFA
